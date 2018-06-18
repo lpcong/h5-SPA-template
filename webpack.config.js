a9921d07cb9884624ede41b3fb54accd35fac950
梁沛聪 <t-cong@163.com>
2018-06-18 15:09:00 +0800

    webpack配置更新，加入图像文件提取和图像压缩功能

--- webpack.config.js
+++ webpack.config.js
@@ -30,14 +30,32 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,  // replace ExtractTextPlugin.extract({..})
                     "css-loader"
-                ]
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg|bmp|eot|woff|woff2|ttf)$/,
+                loader: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5 * 1024, // 图片大小 > limit 使用file-loader, 反之使用url-loader
+                        outputPath: 'img/'// 指定打包后的图片位置
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(html|html)$/,
+                use: 'html-withimg-loader',
+                include: __dirname + "/app/src",
+                exclude: /node_modules/
             }
         ]
     },
     resolve: {
         alias: {
             node_modules : __dirname + '/node_modules',
-            _vue: __dirname + '/node_modules/vue/dist/vue.runtime.esm.js',
+            _vue: __dirname + '/node_modules/vue/dist/vue.esm.js',
             _axios: __dirname + '/node_modules/axios/dist/axios.js'
         }
     },
@@ -45,14 +63,14 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename: "css/[name]-[hash].css",
-            chunkFilename: "css/[id].css"
+            filename: "[name]-[hash].css",
+            chunkFilename: "[id].css"
         }),
         new webpack.BannerPlugin('author by andyp'),
         new HtmlWebpackPlugin({
             template: __dirname + "/app/src/html/index.html"
         }), 
-        new CleanWebpackPlugin(['app/public/*.*','app/public/css/*.*'], {
+        new CleanWebpackPlugin(['app/public/*'], {
             root: __dirname,
             verbose: true,
             dry: false
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
     entry:  __dirname + "/app/src/js/main.js",
@@ -26,13 +27,30 @@ module.exports = {
                     MiniCssExtractPlugin.loader,  // replace ExtractTextPlugin.extract({..})
                     "css-loader"
                 ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|bmp|eot|woff|woff2|ttf)$/,
+                loader: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5 * 1024, // 图片大小 > limit 使用file-loader, 反之使用url-loader
+                        outputPath: 'img/'// 指定打包后的图片位置
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(html|html)$/,
+                use: 'html-withimg-loader',
+                include: __dirname + "/app/src",
+                exclude: /node_modules/
             }
         ]
     },
     resolve: {
         alias: {
             node_modules : __dirname + '/node_modules',
-            _vue: __dirname + '/node_modules/vue/dist/vue.runtime.esm.js',
+            _vue: __dirname + '/node_modules/vue/dist/vue.esm.js',
             _axios: __dirname + '/node_modules/axios/dist/axios.js'
         }
     },
@@ -40,17 +58,24 @@ module.exports = {
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
-            filename: "css/[name]-[hash].css",
-            chunkFilename: "css/[id].css"
+            filename: "[name]-[hash].css",
+            chunkFilename: "[id].css"
         }),
         new webpack.BannerPlugin('author by andyp'),
         new HtmlWebpackPlugin({
             template: __dirname + "/app/src/html/index.html"
         }), 
-        new CleanWebpackPlugin(['app/build/*.*','app/build/css/*.*'], {
+        new CleanWebpackPlugin(['app/build/*'], {
             root: __dirname,
             verbose: true,
             dry: false
+        }),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            disabled: process.env.NODE_ENV !== 'production',
+            pngquant: {
+                quality: '75-80'
+            }
         }),
         new webpack.HotModuleReplacementPlugin()  // 热加载插件
     ],
